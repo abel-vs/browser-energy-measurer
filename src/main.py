95db3c4218ffa87f29364bf13d770194f5c6d45b
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-02-22 12:26:12 +0100

    Add Wikipedia example

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import drivers
 import time
 
-driver = drivers.chrome_driver()
-time.sleep(10)
-driver.quit()
+browser = drivers.chrome_driver()
+browser.get("https://en.wikipedia.org")
+browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+time.sleep(3)
+browser.close()
