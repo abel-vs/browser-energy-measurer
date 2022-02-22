18a955447cd923ed1b03e8f04d2cb8159019063b
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-02-22 12:29:58 +0100

    Added slow scroll

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import drivers
 import time
 
+from mechanisms import slow_scroll
+
 browser = drivers.chrome_driver()
 browser.get("https://en.wikipedia.org")
-browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+# browser.execute_script("window.scrollTo(0,document.body.scrollHeight)") #Immediate scroll
+slow_scroll(browser, 10)
 time.sleep(3)
 browser.close()
---
+++ src/mechanisms.py
@@ -0,0 +1,5 @@
+
+def slow_scroll(driver, time):
+    total_height = int(driver.execute_script("return document.body.scrollHeight"))
+    for i in range(1, total_height, 5):
+        driver.execute_script("window.scrollTo(0, {});".format(i))
