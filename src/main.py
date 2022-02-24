3f35d61db659a75291a345e261fdc1e1d6a4b8ce
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-02-24 16:34:10 +0100

    Added random_wait() function
    For avoiding bot detection.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,12 @@
 import drivers
 import time
-from mechanisms import slow_scroll
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
-from scenarios import wikipedia
+from scenarios import wikipedia, instagram
 
 driver = drivers.chrome_driver()
 
 scenarios = [
-     lambda: wikipedia.visit(driver),
-     lambda: wikipedia.visit(driver),
+     lambda: instagram.visit(driver),
 ]
-
 
 for scenario in scenarios:
     scenario()
