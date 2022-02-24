99d24e8727f90e1e1911cf9ca91ff01b5da66076
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2022-02-24 16:11:06 +0100

    Fix slow scroll
    Now uses speed parameter

--- src/mechanisms.py
+++ src/mechanisms.py
@@ -1,5 +1,5 @@
 
-def slow_scroll(driver, time):
+def slow_scroll(driver, speed):
     total_height = int(driver.execute_script("return document.body.scrollHeight"))
-    for i in range(1, total_height, 5):
+    for i in range(1, total_height, speed):
         driver.execute_script("window.scrollTo(0, {});".format(i))
