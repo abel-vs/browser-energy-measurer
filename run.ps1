fa39dabb05ca511fe4ffa7bbf0a39f3982ada00b
Florentin <arsene.florentin.ichb@gmail.com>
2022-03-02 08:39:17 +0100

    Modify scripts and get results

--- run.ps1
+++ run.ps1
@@ -1,8 +1,39 @@
-$i=1
+sleep 10
+#warm-up runs
+.'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file ".\eval\edge_run_0.csv" -cmd python .\src\edge.py
+.'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file ".\eval\chrome_run_0.csv" -cmd python .\src\chrome.py
+.'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file ".\eval\firefox_run_0.csv" -cmd python .\src\firefox.py
+sleep 30
+$i=0
 While ($i -le 10)
     {
-    $filename = [string]::Format(“edge_run_{0}.csv",$i)
-    .'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file $filename -cmd python .\src\main.py
+    #batch 3 edge runs
+    $j=1
+    While ($j -le 3)
+        {
+        $filename = [string]::Format(“.\eval\edge_run_{0}.csv",$i*3+$j)
+        .'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file $filename -cmd python .\src\edge.py
+        $j++
+        }
+    sleep 30
+    #batch 3 chrome runs
+    $j=1
+    While ($j -le 3)
+        {
+        $filename = [string]::Format(“.\eval\chrome_run_{0}.csv",$i*3+$j)
+        .'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file $filename -cmd python .\src\chrome.py
+        $j++
+        }
+    sleep 30
+    #batch 3 firefox runs
+    $j=1
+    While ($j -le 3)
+        {
+        $filename = [string]::Format(“.\eval\firefox_run_{0}.csv",$i*3+$j)
+        .'C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe' -file $filename -cmd python .\src\firefox.py
+        $j++
+        }
+    sleep 30
     $i++
     }
-   
+ 
---
+++ src/chrome.py
@@ -0,0 +1,25 @@
+import drivers
+import time
+from mechanisms import slow_scroll
+from selenium import webdriver
+
+from scenarios import wikipedia, amazon, google, youtube
+from mechanisms import open_new_tab, wait
+from scenarios import wikipedia, soundcloud, amazon, google
+
+driver = drivers.chrome_driver()
+
+scenarios = [
+     lambda: soundcloud.visit(driver),
+     lambda: wikipedia.visit(driver),
+     lambda: amazon.visit(driver),
+     lambda: youtube.visit(driver),
+     # lambda: google.visit(driver),
+]
+
+for scenario in scenarios:
+    scenario()
+    open_new_tab(driver)
+
+time.sleep(1)
+driver.quit()
